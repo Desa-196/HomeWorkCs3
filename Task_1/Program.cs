cd9f04a387e356748eec1b1e895af446f746ef8d
Денис Пономарев <deskin196@gmail.com>
2023-03-13 12:19:45 +0300

    Написал непосредственно алгоритм проверки на палиндром числа по задаче 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -39,3 +39,28 @@ while(true)
     }
     
 }
+
+//Циклом проходимся по противоположным символам начиная от краев к центру и проверяем одинаковые ли они.
+for(int i=0, k=read_int.Length-1; i <= (read_int.Length/2) - 1; i++, k-- )
+{
+    /*Если символы разные то result = false и выходим из цикла, нет смысла дальше проверять если уже есть различия.
+    А если не попалось ни одного разного символа, то переменная result так и останется true;
+    */
+    if(read_int[i] != read_int[k])
+    {
+        result = false;
+        break;
+    }
+}
+
+if(result)   
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Число {read_int} является полиндромом.");
+}
+else         
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Число {read_int} не является полиндромом.");
+}
+Console.ResetColor();
